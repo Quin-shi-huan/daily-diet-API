fb920121a8ccad75f75e2aafe99ec1526901ba31
Quin-shi-huan <phelipee06@gmail.com>
2025-02-17 09:15:07 -0300

    Create the model Meal

--- models/meal.py
+++ models/meal.py
@@ -1,9 +1,10 @@
 from database import db
+from sqlalchemy.sql import func
 
 
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
-    description = db.Column(db.Text, nullable=False)
-    date_hour = db.Column(db.DateTime, nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    date_hour = db.Column(db.DateTime, nullable=True, default=func.now())
     in_diet = db.Column(db.Boolean, nullable=False)
